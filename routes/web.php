2154701da7e5d3b230e9ea19f4d1a45f0e1e4c20
Niki Patel <ce.nikpatel@gmail.com>
2023-02-23 16:48:53 -0500

    install default auth in laravel project

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,3 +24,6 @@ Route::get('/alpha', function () {
 Route::get('/beta', function () {
     return view('beta');
 });
+
+Auth::routes();
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
